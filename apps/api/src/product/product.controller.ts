88fd22f9ecf1b5a11ed681c9b9f7ee723256bc68
Liri Rexha <lirie.rexhaa@gmail.com>
2026-01-13 17:27:21 +0100

    feat(products): add create product logic

--- apps/api/src/product/product.controller.ts
+++ apps/api/src/product/product.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common'
 import type { Request } from 'express'
 import { ProductService } from './product.service'
 import { RolesGuard } from '../auth/guards/roles.guard'
 import { Roles } from '../auth/decorators/roles.decorator'
 import { Role } from '@prisma/client'
+import { CreateProductDto } from './dto/create-product.dto'
 
 @Controller('products')
 export class ProductController {
@@ -13,6 +14,14 @@ export class ProductController {
   list(@Req() req: Request) {
     const user = (req as any).user as { companyId: number }
     return this.service.list(user.companyId)
+  }
+
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN)
+  @Post()
+  create(@Req() req: Request, @Body() dto: CreateProductDto) {
+    const user = (req as any).user as { companyId: number }
+    return this.service.create(user.companyId, dto)
   }
 
   @UseGuards(RolesGuard)
--- apps/api/src/product/product.service.ts
+++ apps/api/src/product/product.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma/prisma.service'
+import { CreateProductDto } from './dto/create-product.dto'
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
   list(companyId: number) {
     return this.prisma.product.findMany({
       where: { companyId, isActive: true },
-        //TODO: To be discussed
+      //TODO: To be discussed
       orderBy: { createdAt: 'desc' },
+    })
+  }
+
+  async create(companyId: number, dto: CreateProductDto) {
+    return this.prisma.product.create({
+      data: {
+        companyId,
+        name: dto.name,
+        description: dto.description,
+        price: dto.price,
+        isActive: dto.isActive ?? true,
+      },
     })
   }
 }
