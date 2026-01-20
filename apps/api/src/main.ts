880966bccfbaabc17db35f1cbf442a3238497da0
Liri Rexha <lirie.rexhaa@gmail.com>
2026-01-20 20:09:33 +0100

    feat(api): set global prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,6 +14,7 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api') // every controller is under /api/...
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
